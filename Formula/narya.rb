b442662fc2bb3cc9f65238ddad8ef87066cca762
roux g. buciu <github@adudenamedruby.com>
2026-01-16 19:51:29 -0500

    Update release

--- Formula/narya.rb
+++ Formula/narya.rb
@@ -1,17 +1,19 @@
 class Narya < Formula
   desc "CLI tool for managing firefox-ios"
   homepage "https://github.com/adudenamedruby/narya"
-  version "0.3.1"
+  version "20260116.4"
 
   depends_on "swiftlint"
 
+  base_url = "https://github.com/adudenamedruby/narya/releases/download/v#{version}"
+
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/adudenamedruby/narya/releases/download/v0.3.1/narya-v0.3.1-macos-arm64.tar.gz"
-      sha256 "63b8506219f31b499ff9ee8be8f7acd9b9786c9200515b0f0e814ae914c3df2c"
+      url "#{base_url}/narya-v#{version}-macos-arm64.tar.gz"
+      sha256 "e28ba3c893d97af33f6c4088de80d4d339416e7723b6f21ec7a93e5c1f2fa360"
     else
-      url "https://github.com/adudenamedruby/narya/releases/download/v0.3.1/narya-v0.3.1-macos-x86_64.tar.gz"
-      sha256 "679061607c56ed5a5ab482ca9b68ee3f6862334b9057a51354d823e50b6af0fe"
+      url "#{base_url}/narya-v#{version}-macos-x86_64.tar.gz"
+      sha256 "a8514c0d4937e040d170442c35e67df4f5c5c11a08a0498f7affc301ca58b2c9"
     end
   end
 
@@ -20,6 +22,6 @@ class Narya < Formula
   end
 
   test do
-    assert_equal "0.3.1", shell_output("#{bin}/narya --version").strip
+    assert_equal version.to_s, shell_output("#{bin}/narya --version").strip
   end
 end
