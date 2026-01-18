b0bf562d39ce0b2f5cc5f20082cce74f9d9c78bd
roux g. buciu <github@adudenamedruby.com>
2026-01-18 15:08:24 -0500

    New release

--- Formula/narya.rb
+++ Formula/narya.rb
@@ -1,19 +1,20 @@
 class Narya < Formula
   desc "CLI tool for managing firefox-ios"
   homepage "https://github.com/adudenamedruby/narya"
-  version "20260118.0"
+  version "20260118.1"
 
   depends_on "swiftlint"
+  depends_on "node"
 
   base_url = "https://github.com/adudenamedruby/narya/releases/download/v#{version}"
 
   on_macos do
     if Hardware::CPU.arm?
       url "#{base_url}/narya-v#{version}-macos-arm64.tar.gz"
-      sha256 "b17363c8842826470d9d2ea3283aaa75fd20fb26f22c870423bfd8c82f7eb170"
+      sha256 "de170703482233cbdb0376c69c3b0636e3e5a982d345f0e9b90f09e6b1e96ae4"
     else
       url "#{base_url}/narya-v#{version}-macos-x86_64.tar.gz"
-      sha256 "e13c1da282e42b5abdf55ddbbf33d08e608ecd4498421c397c8a7195b1d101b1"
+      sha256 "f211b89388faa12389abcf8595b445fc06e5425205c6d84080c4904c2d4db457"
     end
   end
 
